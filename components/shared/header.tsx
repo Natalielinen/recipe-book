613f83372ce95d8441812d22fb24490ed253a1c5
Natalie <nataliech0409@gmail.com>
2024-08-24 16:26:19 +0300

    header

---
+++ components/shared/header.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import Link from "next/link";
+import { cn } from "../../lib/utils";
+import { Container } from "./container";
+import { useTranslation } from '../../app/i18n/client'
+import { Button } from "../ui/button";
+import { Album, User } from "lucide-react";
+import { ThemeButton } from "./theme-button";
+import { LanguagesSwitcher } from "./languages-switcher";
+
+interface Props {
+    className?: string;
+    lng: string;
+}
+
+export const Header: React.FC<Props> = ({ className, lng }) => {
+    const { t } = useTranslation(lng)
+    return <header className={cn('border border-b', className as string)}>
+        <Container className="flex items-center justify-between py-8">
+            {/* Левая часть */}
+            <Link href="/">
+                <div className="flex items-center gap-4">
+
+                    <div>
+                        <h1 className="text-2xl uppercase font-black">Recipes</h1>
+                        <h1 className="text-2xl uppercase font-black flex gap-1 items-center">Book <Album /></h1>
+                    </div>
+                </div>
+            </Link>
+
+            {/* Поиск */}
+            {/* {
+                hasSearch && <div className="mx-10 flex-1">
+
+                    <SearchInput />
+                </div>
+            } */}
+
+            {/* Правая часть */}
+            <div className="flex items-center gap-3">
+                <ThemeButton />
+                <LanguagesSwitcher lng={lng} />
+                <Button variant="outline">
+                    <User />
+                    {t("Войти")}
+                </Button>
+            </div>
+            {/* <div className="flex items-center gap-3">
+                <AuthModal
+                    open={openAuthModal}
+                    onClose={() => setOpenAuthModal(false)}
+                />
+                <ProfileButton onClickSignIn={() => setOpenAuthModal(true)} />
+                {
+                    hasCart && <div>
+                        <CartButton />
+                    </div>
+                }
+
+            </div> */}
+        </Container>
+    </header>
+};
