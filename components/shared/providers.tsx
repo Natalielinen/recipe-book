15daa0b27a2b38df59b0114860ee694192a98083
Natalie Chufistova <nataliech0409@gmail.com>
2024-11-09 09:32:00 +0300

    fix2

--- components/shared/providers.tsx
+++ components/shared/providers.tsx
@@ -4,9 +4,10 @@ import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react"
 import NextTopLoader from 'nextjs-toploader';
 import { ThemeProvider } from "./theme-provider";
+import {Suspense} from "react";
 export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
     return (
-        <>
+        <Suspense>
             <SessionProvider>
                 <ThemeProvider
                     attribute="class"
@@ -20,6 +21,6 @@ export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
             <Toaster />
             <NextTopLoader />
 
-        </>
+        </Suspense>
     )
 };
