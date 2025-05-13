60e507ceb340f94c9bd3026092633041b3e498cf
Natalie Chufistova <nataliech0409@gmail.com>
2025-05-13 09:28:35 +0300

    user statistics

---
+++ components/shared/profile-header.tsx
@@ -0,0 +1,20 @@
+import { Sparkles } from "lucide-react"
+import { Title } from "./title"
+import { User } from "@prisma/client";
+import React from "react";
+
+interface ProfileHeaderProps {
+    user: User;
+}
+
+export const ProfileHeader: React.FC<ProfileHeaderProps> = ({ user }) => {
+    return (
+        <div className="flex gap-2">
+            <Title text={`${"Личные данные"} | ${user.fullName ? user.fullName : `#${user.id}`}`} size="md" className="font-bold" />
+            {
+                user.vip && <span className="flex font-bold text-2xl text-orange-300">vip <Sparkles size={16} /></span>
+            }
+
+        </div>
+    )
+}
---
+++ components/shared/statistics.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { Api } from "@/app/services/api-client";
+import React, { useState } from "react";
+import { User } from "@prisma/client";
+import Link from "next/link";
+import { Skeleton } from "../ui/skeleton";
+
+interface StatisticsProps {
+    data: User;
+}
+
+type UserStatistics = {
+    recipesCount: number;
+    recipesByCategory: Array<{
+        categoryName: string;
+        recipesCount: number;
+    }>;
+}
+
+export const Statistics: React.FC<StatisticsProps> = ({ data }) => {
+
+    const [statistics, setStatistics] = useState<UserStatistics>({
+        recipesCount: 0,
+        recipesByCategory: []
+    });
+    const [loading, setLoading] = React.useState(false);
+
+    const fetchRecipesInfo = async () => {
+        setLoading(true);
+        const response = await Api.recipes();
+
+        setStatistics({
+            recipesCount: response.reduce((acc, item) => acc + item.recipes.length, 0),
+            recipesByCategory: response.map((el) => ({ categoryName: el.nameKey, recipesCount: el.recipes.length }))
+        })
+
+        setLoading(false);
+    };
+
+    React.useEffect(() => {
+        fetchRecipesInfo();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return <ul className="mt-10 border-l-muted-foreground">
+        <li>
+            {
+                loading
+                    ? <Skeleton className="w-full h-[30px] rounded-2xl mb-1" />
+                    : <Link href={`/recipes`} className="hover:underline hover:text-primary">
+                        Всего рецептов: <b>{statistics.recipesCount}</b>
+                    </Link>
+            }
+
+        </li>
+        <li>Рецептов по категориям:
+            {' '}
+            {
+                loading
+                    ? <Skeleton className="w-full h-[90px] rounded-2xl mb-1" />
+                    : <ul className="ml-4">
+                        {statistics.recipesByCategory.map((el) => <li key={el.categoryName}>
+                            {el.categoryName}: <b>{el.recipesCount}</b>
+                        </li>)}
+                    </ul>
+            }
+
+        </li>
+    </ul>
+
+}
