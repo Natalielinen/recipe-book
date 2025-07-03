f62160e68db4fdd103b01c662c13ae8b931b2f1f
Natalie Chufistova <nataliech0409@gmail.com>
2025-07-03 06:56:22 +0300

    fix

--- lib/upload-image.ts
+++ lib/upload-image.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
-
-const API_KEY = "13b2d0d834fc27d79d6830d6bdb88bbf";
 async function uploadImage(formData: FormData): Promise<any> {
-    return await axios.post(`https://api.imgbb.com/1/upload?key=${API_KEY}`, formData);
+    return await axios.post(`https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMAGEBB_URL}`, formData);
 }
 
 export const onLoadFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
